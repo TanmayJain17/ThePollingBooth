2f94b144039a78516b9c7a5b56cbb677e0fb9e4f
Tanmay <tanmay.jain260@gmail.com>
2021-05-25 20:28:07 +0530

    express-server with socket.io

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const socket = require('socket.io')
+const connection = require('./src/db/connection')
+const route  = require('./src/routes/polls/index')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use('/',express.static(__dirname + '/public'))
+const server = http.createServer(app)
+
+app.use('/api/polls',route)
+
+const io = socket(server)
+
+server.listen(5334,()=>{
+    console.log('listening on http://localhost:5334')
+})
