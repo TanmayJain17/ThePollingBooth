465431b8d19046f9c51a5edaa2e493b0fd866175
Tanmay <tanmay.jain260@gmail.com>
2021-05-27 23:14:27 +0530

    home.js file

---
+++ src/public/app/home.js
@@ -0,0 +1,14 @@
+let btnMakePoll = $('#btnMakePoll')
+btnMakePoll.click(() => {
+    $('#content').load('../components/makePoll.html')
+})
+let btnGoToPoll = $('#btnGoToPoll')
+btnGoToPoll.click(() => {
+    if(!($('#theId').val())){
+        window.alert('enter the poll id')
+    }
+    else{
+        $('#content').data('pollId',$('#theId').val())
+        $('#content').load('../components/pollPage.html') //?id=${$('#theId').val()}
+    }
+})
