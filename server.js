8b1ca6d7c53ed21f382561b78b64d4f8ec7397eb
Tanmay <tanmay.jain260@gmail.com>
2021-05-28 23:20:21 +0530

    socket directly added to the server

--- server.js
+++ server.js
@@ -14,10 +14,48 @@ const server = http.createServer(app)
 app.use('/api/polls', route)
 
 const io = socket(server)
+let pollData = []
+
+io.on('connection', (socket) => {
+    console.log('connection made in id: ', socket.id)
+
+    socket.on('addData', (data) => {
+        pollData.push(data)
+        socket.emit('dataReceived', { totalData: pollData })
+    })
+
+    socket.on('roomJoin', (data) => {
+
+        socket.join(data.roomId)
+
+        socket.emit('roomJoined')
+
+    })
+    socket.on('optionClicked', (data) => {
+
+
+        let theData = pollData.find(ele => ele.pollId == data.roomId)/*.totalPeople =  pollData.find(ele => ele.pollId == data.roomId).totalPeople + 1; */
+        /* let thatFullData = pollData.find(ele => ele.pollId == data.roomId)
+        let totalCount = thatFullData.totalPeople
+        let thatOption = thatFullData.find(opt => opt.theOptions.find(clickId => clickId.id == data.optionId))
+
+        pollData.find(ele => ele.pollId == data.roomId).find(opt => opt.theOptions.find(clickId => clickId.id == data.optionId)).count += 1
+
+        thatOption.count += 1;
+        for (let ele of thatFullData) {
+            ele.theOptions.percent = (ele.theOptions.count / totalCount) * 100
+        } */
+        socket.emit('sendingData', { theData })
+        //io.to(data.roomId).emit('sendingData', { theData })
+    })
+
+
+
+})
+
 
 
 server.listen(5334, () => {
     console.log('listening on http://localhost:5334')
 })
 
-module.exports = {io}
