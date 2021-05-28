1e0e06261c86a06fa084c75ac2bd14fe0cf2f3ed
Tanmay <tanmay.jain260@gmail.com>
2021-05-28 23:22:23 +0530

    error-need to change full socket

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,15 +1,38 @@
 const {io} = require('../../server')
 
+let pollData=[]
+
 io.on('connection',(socket)=>{
     console.log('connection made in id: ',socket.id)
+
+    socket.on('addData',(data)=>{
+        pollData.push(data)
+        socket.emit('dataReceived',{idCreated:data.pollId})
+    })
     
     socket.on('roomJoin',(data)=>{
         
         socket.join(data.roomId)
+        
         socket.emit('roomJoined')
         
     })
-    let dataStored = []
+    socket.on('optionClicked',(data)=>{
+        /* let totalCount = io.sockets.adapter.rooms[data.roomId].length; */
+        
+        let thatFullData = pollData.find(ele=>ele.pollId==data.roomId)
+        
+        thatFullData.totalPeople+=1;
+        let totalCount = thatFullData.totalPeople
+        let thatOption = thatFullData.find(opt=>opt.theOptions.find(clickId=>clickId.id==data.optionId))
+        pollData.find(ele=>ele.pollId==data.roomId).find(opt=>opt.theOptions.find(clickId=>clickId.id==data.optionId)).count+=1
+        thatOption.count+=1;
+        for(let ele of thatFullData){
+            ele.theOptions.percent = (ele.theOptions.count/totalCount)*100
+        }
+        io.to(data.roomId).emit('sendingData',{thatFullData})
+    })
+    /* let dataStored = []
     socket.on('optionClicked',(data)=>{
         let totalCount = io.sockets.adapter.rooms[data.roomId].length;
         if(totalCount==1){
@@ -20,14 +43,14 @@ io.on('connection',(socket)=>{
         thatOption.count +=1;
         for(let eachDataStored of dataStored){
             eachDataStored.push({percent:((eachDataStored.count/totalCount)*100)})
-        }
+        } */
         /* let len = data.otherOptions.length */
-        socket.emit('percentReceived',{
+       /*  socket.emit('percentReceived',{
             impData:dataStored
         })
 
 
-   })
+   }) */
 
     
 })
