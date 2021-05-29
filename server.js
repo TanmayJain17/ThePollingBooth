ddcf650e6aac1219642920a66814b3104f57ff73
Tanmay <tanmay.jain260@gmail.com>
2021-05-29 15:43:10 +0530

    server.js-updated socket.io

--- server.js
+++ server.js
@@ -26,27 +26,34 @@ io.on('connection', (socket) => {
 
     socket.on('roomJoin', (data) => {
 
-        socket.join(data.roomId)
+        socket.join(data.roomId) //socket-this
 
         socket.emit('roomJoined')
 
     })
-    socket.on('optionClicked', (data) => {
+
+    socket.on('btnClicked', (data) => {
+        let clientPollId = data.pollId
+        let clientOptionId = data.optionId
 
 
-        let theData = pollData.find(ele => ele.pollId == data.roomId)/*.totalPeople =  pollData.find(ele => ele.pollId == data.roomId).totalPeople + 1; */
-        /* let thatFullData = pollData.find(ele => ele.pollId == data.roomId)
-        let totalCount = thatFullData.totalPeople
-        let thatOption = thatFullData.find(opt => opt.theOptions.find(clickId => clickId.id == data.optionId))
+        //increasing total count first
+        pollData.find(ele => ele.pollId == clientPollId).totalPeople += 1
+        let totalNoClients = pollData.find(ele => ele.pollId == clientPollId).totalPeople
+        //increasing the count of that option
+        pollData.find(ele => ele.pollId == clientPollId).theOptions.find(ele => ele.id == clientOptionId).count += 1
 
-        pollData.find(ele => ele.pollId == data.roomId).find(opt => opt.theOptions.find(clickId => clickId.id == data.optionId)).count += 1
+        //updating the percent in the real data
+        for (let eachOption of pollData.find(ele => ele.pollId == clientPollId).theOptions) {
+            eachOption.percent = (eachOption.count / totalNoClients) * 100
+        }
+        let thisRoomData = pollData.find(ele => ele.pollId == clientPollId)
+        let dataToSend = thisRoomData.theOptions
 
-        thatOption.count += 1;
-        for (let ele of thatFullData) {
-            ele.theOptions.percent = (ele.theOptions.count / totalCount) * 100
-        } */
-        socket.emit('sendingData', { theData })
-        //io.to(data.roomId).emit('sendingData', { theData })
+        console.log('btnclicked ', 'totalNoClients ', totalNoClients, ' RoomData ', thisRoomData)
+        //send data to every client in the room client
+        io.to(clientPollId).emit('sendingRealData', { dataToSend })
+
     })
 
 
