36138a69f1493aa09ae6484193434126c55dfeb5
Tanmay <tanmay.jain260@gmail.com>
2021-06-11 11:41:04 +0530

    changed uri

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 /* const {pollModel} = require('./modelSchema') */
-const mongoDbUrl = process.env.MONGODB_URL ||'mongodb://localhost:27017/yourpoll'
+const mongoDbUrl = process.env.MONGODB_URI ||'mongodb://localhost:27017/yourpoll'
 mongoose.connect(mongoDbUrl,{useNewUrlParser:true,useUnifiedTopology: true},(error)=>{
     if(error){
         console.log(error)
